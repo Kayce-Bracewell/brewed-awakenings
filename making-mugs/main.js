b5cb2a7f5989a9715f7fb778218d858beb40c0b5
Kayce-Bracewell <Kayceb98@gmail.com>
2024-01-30 13:55:36 -0600

    Branch Testing

---
+++ making-mugs/main.js
@@ -0,0 +1,9 @@
+const clay = [ "Clay", "Clay", "Clay", "Clay" ]
+const toFireInKiln = []
+
+for (let material of clay) {
+   let mug = `${material} coffee mug`
+   toFireInKiln.push(mug)
+}
+
+console.log(toFireInKiln)
